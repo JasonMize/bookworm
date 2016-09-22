4b36b72f64e48f9d5a9811b20c03c2e361bed8cd
Matt Smith <msmith@apaxsoftware.com>
2016-09-22 13:34:45 -0400

    Add book detail page.

--- books/views.py
+++ books/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 
-# Create your views here.
+from .models import Book
+
+def book_detail(request, id):
+    """
+    View for a book
+    """
+    book = get_object_or_404(Book, pk=id)
+
+    context = {
+        "book": book,
+    }
+
+    return render(request, "books/book_detail.html", context)
