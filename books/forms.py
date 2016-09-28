2aed020f54f1359d19df1a75174aeeda3f855a8a
Matt Smith <msmith@apaxsoftware.com>
2016-09-28 08:53:22 -0400

    Enter author names in a text field.

--- books/forms.py
+++ books/forms.py
@@ -5,13 +5,25 @@ from core.forms import BootstrapFormMixin
 
 class BookForm(BootstrapFormMixin, forms.ModelForm):
 
+    author_names = forms.CharField(max_length=500)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance.pk:
+            self.fields['author_names'].initial = self.instance.get_authors()
+
     class Meta:
         model = Book
 
         fields = (
             'title',
             'wikipedia_url',
+            'author_names',
             'bookshelf',
-            'authors',
             'genres',
         )
+
+    def clean_author_names(self):
+        authors_list = [name.strip() for name in self.cleaned_data['author_names'].split(',')]
+
+        return authors_list
--- books/views.py
+++ books/views.py
@@ -83,6 +83,15 @@ def book_new(request, bookshelf=None):
 
         if form.is_valid():
             book = form.save()
+            authors_for_book = []
+            for author_name in form.cleaned_data['author_names']:
+                author, created = Author.objects.get_or_create(name=author_name)
+                authors_for_book.append(author)
+
+            if len(authors_for_book) > 0:
+                book.authors.clear()
+                book.authors.add(*authors_for_book)
+
             messages.success(request, "Book Saved!")
             return redirect("books:book_detail", id=book.pk)
     else:
@@ -119,6 +128,15 @@ def book_edit(request, id):
 
         if form.is_valid():
             book = form.save()
+            authors_for_book = []
+            for author_name in form.cleaned_data['author_names']:
+                author, created = Author.objects.get_or_create(name=author_name)
+                authors_for_book.append(author)
+
+            if len(authors_for_book) > 0:
+                book.authors.clear()
+                book.authors.add(*authors_for_book)
+
             messages.success(request, "Book Saved!")
             return redirect("books:book_detail", id=book.pk)
     else:
