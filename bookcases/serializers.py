eda3780f8461b6632803fcf1bbbedbf45704a701
Matt Smith <msmith@apaxsoftware.com>
2016-10-11 14:26:52 -0400

    Add book count and book search.

--- bookcases/serializers.py
+++ bookcases/serializers.py
@@ -3,13 +3,19 @@ from rest_framework import serializers
 from .models import Bookcase, Bookshelf
 
 class BookshelfSerializer(serializers.ModelSerializer):
+    book_count = serializers.SerializerMethodField()
+
     class Meta:
         model = Bookshelf
         fields = (
             'id',
             'shelf_label',
             'bookcase',
+            'book_count',
         )
+
+    def get_book_count(self, bookshelf):
+        return bookshelf.book_set.count()
 
 class BookcaseSerializer(serializers.ModelSerializer):
     bookshelf_set = BookshelfSerializer(many=True, read_only=True)
--- books/viewsets.py
+++ books/viewsets.py
@@ -1,8 +1,16 @@
 from rest_framework import viewsets
+from django.db.models import Q
 
-from .models import Book
+from .models import Book, Author
 from .serializers import BookSerializer
 
 class BookViewSet(viewsets.ModelViewSet):
-    queryset = Book.objects.all()
     serializer_class = BookSerializer
+    queryset = Book.objects.all()
+
+    def get_queryset(self):
+        query = self.request.query_params.get('q', None)
+        if query:
+            self.queryset = self.queryset.filter(title__icontains = query)
+
+        return self.queryset
