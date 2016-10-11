bbe654545abde3fa3d23cc41ee712003cc0370f2
Matt Smith <msmith@apaxsoftware.com>
2016-10-11 11:41:34 -0400

    Bookshelf endpoint.

--- bookcases/serializers.py
+++ bookcases/serializers.py
@@ -1,8 +1,19 @@
 from rest_framework import serializers
 
-from .models import Bookcase
+from .models import Bookcase, Bookshelf
+
+class BookshelfSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bookshelf
+        fields = (
+            'id',
+            'shelf_label',
+            'bookcase',
+        )
 
 class BookcaseSerializer(serializers.ModelSerializer):
+    bookshelf_set = BookshelfSerializer(many=True, read_only=True)
+
     class Meta:
         model = Bookcase
-        fields = ('id', 'name', 'description', )
+        fields = ('id', 'name', 'description', 'bookshelf_set',)
--- bookcases/viewsets.py
+++ bookcases/viewsets.py
@@ -1,8 +1,12 @@
 from rest_framework import viewsets
 
-from .models import Bookcase
-from .serializers import BookcaseSerializer
+from .models import Bookcase, Bookshelf
+from .serializers import BookcaseSerializer, BookshelfSerializer
 
 class BookcaseViewSet(viewsets.ModelViewSet):
     queryset = Bookcase.objects.all()
     serializer_class = BookcaseSerializer
+
+class BookshelfViewSet(viewsets.ModelViewSet):
+    queryset = Bookshelf.objects.all()
+    serializer_class = BookshelfSerializer
