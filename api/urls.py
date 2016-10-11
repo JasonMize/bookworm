0583fcdd74dc7943c4628b2ed523b3259d1a881f
Matt Smith <msmith@apaxsoftware.com>
2016-10-11 07:56:23 -0400

    API for bookshelves.

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 
-from bookcases.viewsets import BookcaseViewSet
+from bookcases.viewsets import BookcaseViewSet, BookshelfViewSet
 
 router = DefaultRouter()
 router.register(r'bookcases', BookcaseViewSet)
+router.register(r'bookshelves', BookshelfViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
--- bookcases/serializers.py
+++ bookcases/serializers.py
@@ -1,10 +1,16 @@
 from rest_framework import serializers
 
-from .models import Bookcase
+from .models import Bookcase, Bookshelf
+
+class BookshelfSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Bookshelf
+        fields = ('id', 'shelf_label', 'bookcase', )
 
 class BookcaseSerializer(serializers.ModelSerializer):
+    bookshelf_set = BookshelfSerializer(many=True)
+
     class Meta:
         model = Bookcase
-        fields = ('id', 'name', 'description', 'bookshelf_set' ,)
+        fields = ('id', 'name', 'description', 'bookshelf_set',)
         read_only_fields = ('bookshelf_set', )
-        depth = 1
